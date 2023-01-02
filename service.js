a8d19a6419d11c869084cff80869ef1f5d3447f3
Alexander Lima <alexanderlima2008@gmail.com>
2023-01-02 11:23:23 -0300

    added sp_id in req

--- service.js
+++ service.js
@@ -3,7 +3,7 @@ var Service = require('node-windows').Service;
 var svc = new Service({
      name:'App-consulta',
      description: 'Runs App-consulta permanently.',
-     script: 'I:\\Geral\\31 App Consulta\\app.js'
+     script: 'I:\\Geral\\31 App Consulta\\App-Consulta\\app.js'
 });
 
 svc.on('install', () => { svc.start() });
