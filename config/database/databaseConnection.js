2c961e67e43c7aa4fcf32b31e916d7a2f51386fe
Alexander <alexanderlima2008@gmail.com>
2023-10-10 16:12:43 -0300

    change db to new location

--- config/database/databaseConnection.js
+++ config/database/databaseConnection.js
@@ -1,8 +1,9 @@
-const sqlite = require('sqlite3');
+const sqlite = require('sqlite3').verbose();
 
 module.exports = function () {
     this.db = function () {
-        return new sqlite.Database("./config/database/Sqlite3/Main.db")
+        const dbPath = "\\\\192.168.1.202\\dados novo servidor\\01 Geral\\28 App Consulta DB\\Main.db"
+        return new sqlite.Database(dbPath)
     }
     return this
 }
--- service.js
+++ service.js
@@ -3,7 +3,7 @@ var Service = require('node-windows').Service;
 var svc = new Service({
      name:'App-consulta',
      description: 'Runs App-consulta permanently.',
-     script: 'I:\\Geral\\31 App Consulta\\App-Consulta\\app.js'
+     script: 'E:\\Aplicativos\\App-consulta\\app.js'
 });
 
 svc.on('install', () => { svc.start() });
