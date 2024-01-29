9986a857012f0fab48b1c229add5506ae68491c5
Alexander-Lima <alexanderlima2008@gmail.com>
2024-01-29 12:54:20 -0300

    add error handling to simples route

--- app/routes/Consulta-simples-nacional.js
+++ app/routes/Consulta-simples-nacional.js
@@ -1,12 +1,16 @@
 module.exports = function (app) {
     app.get('/consulta-simples/:clientId', async (req, res) => {
-        const clientId = req.params.clientId;
-        if(isNaN(clientId)) {
-            return res.status(400).end();
+        try {
+            const clientId = req.params.clientId;
+            if(isNaN(clientId)) {
+                return res.status(400).end();
+            }
+            const Service = app.app.services.ConsultaSimplesNacionalService
+            const result = await Service.isOptanteSimples(req.params.clientId);
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({ isOptante: result }));
+        } catch (e) {
+            res.status(400).end(e.message ? e.message : e)
         }
-        const Service = app.app.services.ConsultaSimplesNacionalService
-        const result = await Service.isOptanteSimples(req.params.clientId);
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ isOptante: result }));
     })
 }
