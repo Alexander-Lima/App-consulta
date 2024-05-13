ee9fcef9db0088cd8b259a7e62e5d5b12996d3fb
Alexander-Lima <alexanderlima2008@gmail.com>
2024-05-13 16:10:02 -0300

    fix login bug

--- app/services/AuthenticationService.js
+++ app/services/AuthenticationService.js
@@ -2,11 +2,15 @@ const bcrypt = require('bcrypt');
 
 module.exports = function() {
     this.authenticate = async (DAO, user, pass) => {
-        return new Promise(async (res, rej) => {
-            const { hash }  = await DAO.getUserPasswordHash(user)
+        return new Promise(async (res) => {
+            const resp  = await DAO.getUserPasswordHash(user)
+            if(!resp) {
+                return res(false)
+            }
+            const { hash } = resp
             bcrypt.compare(pass, hash, (err, result) => {
                 if(err) {
-                    rej(err)
+                    rej(false)
                 }
                 res(result)
             })
